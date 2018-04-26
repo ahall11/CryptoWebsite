df68ff89cf10398c44371ad4162bf63c6357eb4c
Alex <a_hall11@u.pacific.edu>
2018-04-25 17:25:27 -0700

    update

--- database.php
+++ database.php
@@ -1,3 +1,20 @@
 <?php
-	$db = pg_connect("host=alexanderjhall.com dbname=crypto user=postgres password=8amlol");
+	require __DIR__ . '/vendor/autoload.php';
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->required(['DATABASE_URL']);
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+	$dbopts["path"] = ltrim($dbopts["path"], "/");
+	print_r ($dbopts);
+	$dbopts["path"] = ltrim($dbopts["path"], "/");
+	$dbopts["path"] = ltrim($dbopts["path"], "/");
+
+	$db = pg_connect(sprintf(
+	"host=%s dbname=%s user=%s password=%s",
+	$dbopts["host"],
+	$dbopts["user"],
+	$dbopts["pass"],
+	ltrim($dbopts["path"], "/")
+	));
+
+	// $db = pg_connect("host=10.0.0.86 dbname=crypto user=postgres password=8amlol");
 ?>

91416ec2643e63d13aaa09325f5fde7d0be86838
Alex <a_hall11@u.pacific.edu>
2018-04-26 22:55:45 -0700

    update

--- database.php
+++ database.php
@@ -2,7 +2,10 @@
 		// $db = pg_connect("host=ec2-54-83-204-6.compute-1.amazonaws.com dbname=d6isj2vtbas9mb user=omlzkasubunfgk password=17713e4e401705421e1b1c66c3864834f09515f3583d9a46cb25cdfbd0be5661");
 	require __DIR__ . '/vendor/autoload.php';
 	$dbopts = parse_url(getenv('DATABASE_URL'));
-	print_r ($dbopts);
-	$dbopts["path"] = ltrim($dbopts["path"], "/");
+	// print_r ($dbopts);
+	print $dbopts["host"];
+	print $dbopts["user"];
+	print $dbopts["pass"];
+	print ltrim($dbopts["path"], "/");
 	$db = pg_connect(sprintf("host=%s dbname=%s user=%s password=%s", $dbopts["host"], $dbopts["user"], $dbopts["pass"], ltrim($dbopts["path"], "/")));
 ?>
